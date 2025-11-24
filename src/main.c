77213d4f371b3d4ea112a8412bc19f90ef4af199
MarioR17 <jmrendonq17@gmail.com>
2025-11-23 23:21:05 -0500

    [any message]

--- src/main.c
+++ src/main.c
@@ -1,12 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-int main(int argc, char *argv[]) {
-  // Flush after every printf
-  setbuf(stdout, NULL);
+int main(int argc, char *argv[])
+{
+        char buffer[128];
 
-  // TODO: Uncomment the code below to pass the first stage
-  printf("$ ");
+        // Flush after every printf
+        setbuf(stdout, NULL);
 
-  return 0;
+        if (fgets(buffer, sizeof(buffer), stdin) == NULL) {
+                printf("ERROR: Could not read input command\n");
+        }
+
+        buffer[strlen(buffer) - 1] = '\0';
+
+        // TODO: Uncomment the code below to pass the first stage
+        printf("$ ");
+
+        printf("\n%s: command not found\n", buffer);
+
+        return 0;
 }
