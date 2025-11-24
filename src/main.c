f543ad85407a65b056d8febfe6d10e0d93b1fc6f
MarioR17 <jmrendonq17@gmail.com>
2025-11-23 23:22:42 -0500

    Fix text failure

--- src/main.c
+++ src/main.c
@@ -9,16 +9,15 @@ int main(int argc, char *argv[])
         // Flush after every printf
         setbuf(stdout, NULL);
 
+        printf("$ ");
+
         if (fgets(buffer, sizeof(buffer), stdin) == NULL) {
                 printf("ERROR: Could not read input command\n");
         }
 
         buffer[strlen(buffer) - 1] = '\0';
 
-        // TODO: Uncomment the code below to pass the first stage
-        printf("$ ");
-
-        printf("\n%s: command not found\n", buffer);
+        printf("%s: command not found\n", buffer);
 
         return 0;
 }
